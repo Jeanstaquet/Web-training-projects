00d7833036919c31cbeced1cf9ac92396427df75
Jeansta <jean.staquet8@gmail.com>
2020-11-02 01:46:08 +0100

    Bonne avancée du projet, bientôt à la demi

--- whatsapp-clone-ytb/src/App.js
+++ whatsapp-clone-ytb/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React, { Component } from "react"
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Sidebar from "./Components/UI/Sidebar/Sidebar";
+import Chat from "./Containers/Chat/Chat";
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <div className="app__body">
+          <Sidebar/>
+          <Chat />
+        </div>
+      </div>
+    )
+  }
 }
-
 export default App;
---
+++ whatsapp-clone-ytb/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCmhvYiZbaWvXzf0poLYLmSW__qk1G63V8",
+    authDomain: "react-burger-app-ffe1a.firebaseapp.com",
+    databaseURL: "https://react-burger-app-ffe1a.firebaseio.com",
+    projectId: "react-burger-app-ffe1a",
+    storageBucket: "react-burger-app-ffe1a.appspot.com",
+    messagingSenderId: "1045421883272",
+    appId: "1:1045421883272:web:fda043433912a791b5043d",
+    measurementId: "G-STKS5KGN7J"
+  };
