9b25446bf7699bbba21e5241e74369f4d724ff0a
Jeansta <jean.staquet8@gmail.com>
2020-11-06 19:50:40 +0100

    passer des props entre les component

--- hook-training/hooks-03-passing-data-manage-ingredients/src/components/Ingredients/Ingredients.js
+++ hook-training/hooks-03-passing-data-manage-ingredients/src/components/Ingredients/Ingredients.js
@@ -9,17 +9,18 @@ const Ingredients = () => {
 
   const addIngredientHandler = ingredient => {
     setUserIngredients(prevIngredients => [
-      ...prevIngredients, {id: Math.random().toString(), ...ingredient}
-    ])
-  }
+      ...prevIngredients,
+      { id: Math.random().toString(), ...ingredient }
+    ]);
+  };
 
   return (
     <div className="App">
-      <IngredientForm onAddIngredient={addIngredientHandler}/>
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
-        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}}/>
+        <IngredientList ingredients={userIngredients} onRemoveItem={() => {}} />
       </section>
     </div>
   );
