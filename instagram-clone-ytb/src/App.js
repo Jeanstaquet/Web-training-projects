060b944e8a9a0454feac0cd8386954294c3cc0b9
Jeansta <jean.staquet8@gmail.com>
2020-11-11 01:25:37 +0100

    Création du projet, set-up de FB

--- instagram-clone-ytb/src/App.js
+++ instagram-clone-ytb/src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Post from './Post';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username:"cleverqazi", 
+      caption:"WOW it works", 
+      imageUrl:"https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg"
+    },
+    {
+      username:"cleverqazi", 
+      caption:"WOW it works", 
+      imageUrl:"https://upload.wikimedia.org/wikipedia/commons/9/99/Unofficial_JavaScript_logo_2.svg"
+    }
+  ])
+
   return (
+    //BEM convention
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app__header">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/2/2a/Instagram_logo.svg" alt=""/>
+      </div>
+      {
+        posts.map(post => (
+          <Post username={post.username} caption={post.caption} imageUrl={post.imageUrl}/>
+        ))
+      }
+
     </div>
   );
 }
