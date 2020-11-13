20076bc07704cd80d97866a24ed5aeca26f9ee8c
Jeansta <jean.staquet8@gmail.com>
2020-11-13 02:20:40 +0100

    fin du projet, bcp de bonne chose sur les db

--- instagram-clone-ytb/src/Post.js
+++ instagram-clone-ytb/src/Post.js
@@ -57,13 +57,13 @@ function Post({postId, user, username, caption, imageUrl}) {
                     ))
                 }
             </div>
-
-            <form className="post__commentBox">
+            {user && <form className="post__commentBox">
                 <input type="text" className="post__input" placeholder="Add a comment..." value={comment} onChange={(e) => setComment(e.target.value)}/>
                 <button className="post__button" disabled={!comment} type="submit" onClick={postComment}>
                     Post
                 </button>
-            </form>
+            </form>}
+
         </div>
     );
 }
