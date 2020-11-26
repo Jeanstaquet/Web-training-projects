33d7277d33e64fabe5ca011cfaf5b94c2a5d12d6
Jeansta <jean.staquet8@gmail.com>
2020-11-27 00:13:44 +0100

    Recommencement du projet à ce point, pour voir la solution du professeur

--- memory-game-redux/src/components/UI/Playboard/Playboard.js
+++ memory-game-redux/src/components/UI/Playboard/Playboard.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Playboard.scss";
 import Card from "./Card/Card";
 
 function Playboard(props) {
-    const [shouldTurn, setShouldturn] = useState(false)
+    const [previousCard, setPreviousCard] = useState(0);
+    const [count, setCount] = useState(1)
+    const [currentCard, setCurrentCard] = useState(0)
     const [cards, setCards] = useState(
             [{number: 1, turn: false, finded: false},
             {number: 1, turn: false, finded: false},
@@ -24,13 +26,47 @@ function Playboard(props) {
             {number: 9, turn: false, finded: false},
             {number: 9, turn: false, finded: false}]
             )
-    
+
+    let b = 0;
+    useEffect(() => {
+        b = 0
+        console.log(previousCard, currentCard);
+        if(previousCard === currentCard) {
+            for(let i = 0; i < cards.length; i++) {
+                if(Object.values(cards[i])[0] === previousCard) {
+                    b = 1
+                    const a = [...cards];
+                    a[i].turn = true;
+                    a[i - 1].turn = true;
+                    setCards(a)
+                }
+            }
+        }
+    }, [previousCard, currentCard])
+
     const swipeHandler = (id) => {
-        console.log('ok');
         const newState = [...cards];
         newState[id].turn = true;
-        setCards(newState)
+        if(count === 1) {
+            setPreviousCard(() => newState[id].number);
+            console.log(newState[id].number)
+            setCount(count + 1);
+        } else {
+            setCurrentCard(newState[id].number);
+            setCount(1);
+        }
+
+        setCards(newState);
+        const timeOutState = [...newState]
+        setTimeout(() => {
+            if(b !== 1) {
+                timeOutState[id].turn = false;
+                setCards(timeOutState)
+            }
+
+        }, 2000);
     }
+
 
     return (
         <div className="playboard__container">
