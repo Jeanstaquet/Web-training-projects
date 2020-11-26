f5e15e1aaaa2a9675daea6baae8e75d94e9de926
Jeansta <jean.staquet8@gmail.com>
2020-11-26 19:40:42 +0100

    Swipe handler fonctionne

--- memory-game-redux/src/components/UI/Playboard/Card/Card.js
+++ memory-game-redux/src/components/UI/Playboard/Card/Card.js
@@ -2,12 +2,13 @@ import React from 'react';
 import "./Card.scss"
 
 function Card(props) {
-    let classes = ["flipCard__inner", props.doTurn ? "rot": null];
+    let classes = ["flipCard__inner", props.turn ? "rot": null];
     return (
-        <div className="flipCard" onClick={props.turn}>
+        <div className="flipCard" onClick={props.click}>
             <div className={classes.join(" ")}>
                 <div className="flipCard__front">
-                    <img src="https://picsum.photos/200/300" alt="ss"/>
+                    <div className="img"></div>
+                    {/* <img src="https://picsum.photos/200/300" alt="ss"/> */}
                 </div>
                 <div className="flipCard__back">
                     <h1>{props.number}</h1>
--- memory-game-redux/src/components/UI/Playboard/Playboard.js
+++ memory-game-redux/src/components/UI/Playboard/Playboard.js
@@ -4,15 +4,43 @@ import Card from "./Card/Card";
 
 function Playboard(props) {
     const [shouldTurn, setShouldturn] = useState(false)
-
-    const flipHandler = () => {
-        setShouldturn(!shouldTurn)
+    const [cards, setCards] = useState(
+            [{number: 1, turn: false, finded: false},
+            {number: 1, turn: false, finded: false},
+            {number: 2, turn: false, finded: false},
+            {number: 2, turn: false, finded: false},
+            {number: 3, turn: false, finded: false},
+            {number: 3, turn: false, finded: false},
+            {number: 4, turn: false, finded: false},
+            {number: 4, turn: false, finded: false},
+            {number: 5, turn: false, finded: false},
+            {number: 5, turn: false, finded: false},
+            {number: 6, turn: false, finded: false},
+            {number: 6, turn: false, finded: false},
+            {number: 7, turn: false, finded: false},
+            {number: 7, turn: false, finded: false},
+            {number: 8, turn: false, finded: false},
+            {number: 8, turn: false, finded: false},
+            {number: 9, turn: false, finded: false},
+            {number: 9, turn: false, finded: false}]
+            )
+    
+    const swipeHandler = (id) => {
+        console.log('ok');
+        const newState = [...cards];
+        newState[id].turn = true;
+        setCards(newState)
     }
 
     return (
         <div className="playboard__container">
-            <Card number="1" turn={flipHandler} doTurn={shouldTurn}/>
-            <Card number="1" turn={flipHandler} doTurn={shouldTurn}/>
+            {cards.map((card, index) => {
+                return <Card 
+                            number={card.number} 
+                            turn={card.turn} 
+                            key={index} 
+                            click={() => swipeHandler(index)}/>
+            })}
         </div>
     );
 }
