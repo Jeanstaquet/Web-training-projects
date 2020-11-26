8c25cd71f3305b1f9f0584a2232b104a55e41e2c
Jeansta <jean.staquet8@gmail.com>
2020-11-26 01:28:07 +0100

    fin de l'exo sur Redux

--- redux-training/src/store/reducers/order.js
+++ redux-training/src/store/reducers/order.js
@@ -6,52 +6,87 @@ const initalState = {
     purchased: false
 }
 
+const purchaseInit = (state, action) => {
+    return updateObject(state, {purchased: false}) 
+}
 
+const purchaseBurgerStart = (state, action) => {
+    return updateObject(state, {loading: true})
+}
 
+const purchaseBurgerSuccess = (state, action) => {
+    const newOrder = updateObject(action.orderData, {id: action.orderId,purchased: true})
+    return updateObject(state, {loading: false, orders: state.orders.concat(newOrder)})
+}
+
+const purchaseBurgerFail = (state, action) => {
+    return updateObject(state, {loading: false})
+}
+
+const purchaseBurgerstart = (state, action) => {
+    return updateObject(state, {loading: true})
+
+}
+
+const pruchaseBurgerSucess = (state, action) => {
+    return updateObject(state, {orders: action.orders, loading: false})
+
+}
+
+const purchaseFetchOrdersFail = (state, action) => {
+    return updateObject(state, {loading: false})
+}
 const reducer = (state = initalState, action) => {
     switch(action.type) {
         case actionTypes.PURCHASE_INIT:
-            return updateObject(state, {purchased: false})
+            return purchaseInit(state, action);
+            // return updateObject(state, {purchased: false})
         case  actionTypes.PURCHASE_BURGER_START:
-            return updateObject(state, {loading: true})
+            return purchaseBurgerStart(state, action)
+            // return updateObject(state, {loading: true})
             // return {
             //     ...state, 
             //     loading: true
             // }
         case actionTypes.PURCHASE_BURGER_SUCCESS:
-            const newOrder = updateObject(action.orderData, {id: action.orderId,purchased: true})
+            return purchaseBurgerSuccess(state, action)
+            // const newOrder = updateObject(action.orderData, {id: action.orderId,purchased: true})
             // const newOrder = {
             //     ...action.orderData,
             //     id: action.orderId,
             //     purchased: true
             // }
-            return updateObject(state, {loading: false, orders: state.orders.concat(newOrder)})
+            // return updateObject(state, {loading: false, orders: state.orders.concat(newOrder)})
             // return {
             //     ...state,
             //     loading: false,
             //     orders: state.orders.concat(newOrder)
             // }
         case actionTypes.PURCHASE_BURGER_FAIL:
-            return updateObject(state, {loading: false})
+            return purchaseBurgerFail(state, action)
+            // return updateObject(state, {loading: false})
             // return {
             //     ...state,
             //     loading: false
             // }
         case actionTypes.FETCH_ORDERS_START:
-            return updateObject(state, {loading: true})
+            return purchaseBurgerstart(state, action)
+            // return updateObject(state, {loading: true})
             // return {
             //     ...state,
             //     loading: true 
             // }
         case actionTypes.FETCH_ORDERS_SUCCESS:
-            return updateObject(state, {orders: action.orders, loading: false})
+            return pruchaseBurgerSucess(state, action);
+            // return updateObject(state, {orders: action.orders, loading: false})
             // return {
             //     ...state, 
             //     orders: action.orders,
             //     loading: false
             // }
         case actionTypes.FETCH_ORDERS_FAIL:
-            return updateObject(state, {loading: false})
+            return purchaseFetchOrdersFail(state, action);
+            // return updateObject(state, {loading: false})
             // return {
             //     ...state,
             //     loading: false
