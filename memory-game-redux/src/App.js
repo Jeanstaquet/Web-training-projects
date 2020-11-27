61df6f7c967e3d5f633adc1e6767affeea6fe71c
Jeansta <jean.staquet8@gmail.com>
2020-11-27 14:05:04 +0100

    Ajout du managing du state avec les lifecycle methods et useEffect pour changer le deck de carte

--- memory-game-redux/src/App.js
+++ memory-game-redux/src/App.js
@@ -1,11 +1,51 @@
 import './App.css';
+import React, {useState, useEffect} from "react"
 import NavBar from "./components/UI/NavBar/NavBar";
 import PlayBoard from "./components/UI/Playboard/Playboard";
 function App() {
+  const [currentCard, setCurrentCard] = useState("");
+  const [nextCard, setNextCard] = useState("");
+  const [find, setFind] = useState(false)
+  const [cards, setCards] = useState(
+          [{number: 1, turn: false, finded: false},
+          {number: 1, turn: false, finded: false},
+          {number: 2, turn: false, finded: false},
+          {number: 2, turn: false, finded: false},
+          {number: 3, turn: false, finded: false},
+          {number: 3, turn: false, finded: false},
+          {number: 4, turn: false, finded: false},
+          {number: 4, turn: false, finded: false},
+          {number: 5, turn: false, finded: false},
+          {number: 5, turn: false, finded: false},
+          {number: 6, turn: false, finded: false},
+          {number: 6, turn: false, finded: false},
+          {number: 7, turn: false, finded: false},
+          {number: 7, turn: false, finded: false},
+          {number: 8, turn: false, finded: false},
+          {number: 8, turn: false, finded: false},
+          {number: 9, turn: false, finded: false},
+          {number: 9, turn: false, finded: false}]
+          )
+
+    useEffect(() => {
+      if(currentCard === nextCard) {
+        setCards([...cards, cards[0].number = currentCard])
+      }
+      console.log(cards[0].number)
+    }, [currentCard, nextCard])
+
+  const swipeHandler = (id) => {
+    setCurrentCard(id);
+    setNextCard(id)
+    console.log(currentCard)
+  }
+
   return (
     <div className="App">
       <NavBar>
-        <PlayBoard/>
+  <h2 style={{textAlign:"center", width:"100%"}}>{currentCard} ///// {nextCard}</h2>
+        <button onClick={() => setCurrentCard(currentCard + 1)}>Push</button>
+        <PlayBoard clicked={swipeHandler} cards={cards}/>
       </NavBar>
     </div>
   );
--- memory-game-redux/src/components/UI/Playboard/Playboard.js
+++ memory-game-redux/src/components/UI/Playboard/Playboard.js
@@ -3,79 +3,14 @@ import "./Playboard.scss";
 import Card from "./Card/Card";
 
 function Playboard(props) {
-    const [previousCard, setPreviousCard] = useState(0);
-    const [count, setCount] = useState(1)
-    const [currentCard, setCurrentCard] = useState(0)
-    const [cards, setCards] = useState(
-            [{number: 1, turn: false, finded: false},
-            {number: 1, turn: false, finded: false},
-            {number: 2, turn: false, finded: false},
-            {number: 2, turn: false, finded: false},
-            {number: 3, turn: false, finded: false},
-            {number: 3, turn: false, finded: false},
-            {number: 4, turn: false, finded: false},
-            {number: 4, turn: false, finded: false},
-            {number: 5, turn: false, finded: false},
-            {number: 5, turn: false, finded: false},
-            {number: 6, turn: false, finded: false},
-            {number: 6, turn: false, finded: false},
-            {number: 7, turn: false, finded: false},
-            {number: 7, turn: false, finded: false},
-            {number: 8, turn: false, finded: false},
-            {number: 8, turn: false, finded: false},
-            {number: 9, turn: false, finded: false},
-            {number: 9, turn: false, finded: false}]
-            )
-
-    let b = 0;
-    useEffect(() => {
-        b = 0
-        console.log(previousCard, currentCard);
-        if(previousCard === currentCard) {
-            for(let i = 0; i < cards.length; i++) {
-                if(Object.values(cards[i])[0] === previousCard) {
-                    b = 1
-                    const a = [...cards];
-                    a[i].turn = true;
-                    a[i - 1].turn = true;
-                    setCards(a)
-                }
-            }
-        }
-    }, [previousCard, currentCard])
-
-    const swipeHandler = (id) => {
-        const newState = [...cards];
-        newState[id].turn = true;
-        if(count === 1) {
-            setPreviousCard(() => newState[id].number);
-            console.log(newState[id].number)
-            setCount(count + 1);
-        } else {
-            setCurrentCard(newState[id].number);
-            setCount(1);
-        }
-
-        setCards(newState);
-        const timeOutState = [...newState]
-        setTimeout(() => {
-            if(b !== 1) {
-                timeOutState[id].turn = false;
-                setCards(timeOutState)
-            }
-
-        }, 2000);
-    }
-
-
     return (
         <div className="playboard__container">
-            {cards.map((card, index) => {
+            {props.cards.map((card, index) => {
                 return <Card 
                             number={card.number} 
                             turn={card.turn} 
                             key={index} 
-                            click={() => swipeHandler(index)}/>
+                            click={() => props.clicked(index)}/>
             })}
         </div>
     );
