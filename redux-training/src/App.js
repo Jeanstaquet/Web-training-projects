e74d386bc25677be4407f19a3758dff76ba4f4a0
Jeansta <jean.staquet8@gmail.com>
2020-12-07 02:20:53 +0100

    Ajout des guards et des redirect pouyr chaque page

--- redux-training/src/App.js
+++ redux-training/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import Auth from "./containers/Auth/Auth";
 import Layout from './hoc/Layout/Layout';
 import {connect} from "react-redux";
@@ -10,18 +10,38 @@ import * as actions from "./store/actions/index";
 
 class App extends Component {
   render () {
+    let routes = (
+      <Switch>
+        <Route path="/" exact component={Auth} />
+        <Redirect to="/"/>
+      </Switch>
+
+    );
+
+    if(this.props.isAuth) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={Logout}/>
+          <Route path="/" exact component={Auth} />
+          <Redirect to="/"/>
+      </Switch>
+      )
+    }
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/logout" component={Logout}/>
-            <Route path="/" exact component={Auth} />
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
+  }
+}
+
+const mpaStateToProps = state => {
+  return {
+    isAuth: state.auth.token !== null
   }
 }
 
@@ -31,4 +51,4 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mpaStateToProps, mapDispatchToProps)(App));
