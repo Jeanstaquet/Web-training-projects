bd0926b2a411ef791bd4c54183b13ef7d81cdd37
Jeansta <jean.staquet8@gmail.com>
2020-12-08 17:16:05 +0100

    Ajout de nouvelle carte fonctionne

--- fullstack-blackjack/src/components/Card/Card.js
+++ fullstack-blackjack/src/components/Card/Card.js
@@ -4,7 +4,8 @@ import "./Card.scss";
 function Card(props) {
     return (
         <div className="card">
-            
+            <h4>{props.cardValues}</h4>
+            <h4>{props.suits}</h4>
         </div>
     );
 }
--- fullstack-blackjack/src/containers/Board/Board.js
+++ fullstack-blackjack/src/containers/Board/Board.js
@@ -1,10 +1,18 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import "./Board.scss"
 import Card from "../../components/Card/Card";
 
 const Board = (props) => {
     const [suits, setSuits] = useState(["spades", "diamonds", "clubs", "hearts"]);
-    const [cardValues, setCardValues] = useState(["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"])
+    const [cardValues, setCardValues] = useState(["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]);
+    const [cardDealer, setCardDealer] = useState([]);
+    const [cardPlayer, setCardPlayer] = useState([]);
+
+    const newCardHandler = () => {
+        let randomSuits =  suits[Math.floor(Math.random() * suits.length)];
+        let randomCardVal = cardValues[Math.floor(Math.random() * cardValues.length)];
+        setCardPlayer([...cardPlayer, {cardVal: randomCardVal, suits: randomSuits}])
+    }
 
     return (
         <div className="board__container">
@@ -15,15 +23,17 @@ const Board = (props) => {
                         <p>💰 Money available: <span className="moneyAvailable">1500$</span></p>
                     </div>
                     <div className="card__container">
-                        <Card/>
+                        {cardPlayer.map((c, index) => {
+                            return <Card key={index} cardValues={c.cardVal} suits={c.suits}/>
+                        })}
                     </div>
                     <div className="player__button">
                         <h2>Acutal Points: XX</h2>
                         <div>
                         <button>💲 Bet</button>
                         <label>Amount:</label>
-                        <input type="number" step="25" placeholder="Insert money"/>
-                        <button>Stop</button>
+                        <input type="number" step="25" placeholder="Insert money" min="0"/>
+                        <button onClick={newCardHandler}>Card</button>
                         <button>New Game</button>
                         </div>
 
@@ -35,7 +45,9 @@ const Board = (props) => {
                         <p>💰 Cash available in the bank: <span className="moneyAvailable">1M$</span></p>
                     </div>
                     <div className="card__container">
-                        
+                    {cardDealer.map((c, index) => {
+                            return <Card key={index} cardValues={c.cardVal} suits={c.suits}/>
+                        })}
                         </div>
                     <div className="dealer__button">
                         <h2>Actual Points: XX</h2>
