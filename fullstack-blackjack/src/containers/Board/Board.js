61ebd639db8b37778ce588284d01534b43682e72
Jeansta <jean.staquet8@gmail.com>
2020-12-11 02:33:45 +0100

    Resolution du problème avec redux, problème quand je définissait le acehandler dans les props, à vérifier

--- fullstack-blackjack/src/containers/Board/Board.js
+++ fullstack-blackjack/src/containers/Board/Board.js
@@ -1,103 +1,100 @@
-import React, { useCallback, useEffect, useReducer } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import "./Board.scss"
 import Card from "../../components/Card/Card";
 import Banner from "../../components/UI/Banner";
 import {connect} from "react-redux"
 
 const Board = (props) => {
-    //Reducer state
-    const [state, dispatch] = useReducer(reducer, initalState);
 
-
-    const cardDistributor = useCallback((nbr) => {
+    const cardDistributor = (nbr) => {
         const arr = []
         for(let i = 0; i < nbr; i++) {
-            let randomSuits =  state.suits[Math.floor(Math.random() * state.suits.length)];
-            let randomCardVal = state.cardValues[Math.floor(Math.random() * state.cardValues.length)];
+            let randomSuits =  props.suits[Math.floor(Math.random() * props.suits.length)];
+            let randomCardVal = props.cardValues[Math.floor(Math.random() * props.cardValues.length)];
 
             arr.push({cardVal: randomCardVal, suits: randomSuits})
             if(randomCardVal === "A") {
-                dispatch({type: "ACE_APPEARED"})
+                props.aceAppeardHandler()
             }   
         }
         return arr
-    }, [state.suits, state.cardValues])
+    }
 
 
-    const playerPointsHandler = useCallback(() => {
+    const playerPointsHandler = () => {
         console.log("player")
         let points = 0
-        for(let i = 0; i < state.cardPlayer.length; i++) {
-            if(!isNaN(state.cardPlayer[i].cardVal)) {
-                points += Number(state.cardPlayer[i].cardVal)
-            } else if(state.cardPlayer[i].cardVal === "A"){
-                points += state.aceValue
+        for(let i = 0; i < props.cardPlayer.length; i++) {
+            if(!isNaN(props.cardPlayer[i].cardVal)) {
+                points += Number(props.cardPlayer[i].cardVal)
+            } else if(props.cardPlayer[i].cardVal === "A"){
+                points += props.aceValue
             } else {
                 points += 10
             }
         }
         let dealerPoint = 0;
-        for(let i = 0; i < state.cardDealer.length; i++) {
-            if(!isNaN(state.cardDealer[i].cardVal)) {
-                dealerPoint += Number(state.cardDealer[i].cardVal)
-            } else if(state.cardDealer[i].cardVal === "A"){
-                dealerPoint += state.aceValue;
+        for(let i = 0; i < props.cardDealer.length; i++) {
+            if(!isNaN(props.cardDealer[i].cardVal)) {
+                dealerPoint += Number(props.cardDealer[i].cardVal)
+            } else if(props.cardDealer[i].cardVal === "A"){
+                dealerPoint += props.aceValue;
             } else {
                 dealerPoint += 10
             }
         }
-        dispatch({type: "DEALER_POINTS", payload: dealerPoint})
-        dispatch({type:"PLAYER_POINTS", payload: points})
+        props.dealerPointsHandler(dealerPoint)
+        props.playerPointsHandler(points)
         if(points > 21) {
-            dispatch({type: "LOST"})
-        } else if(state.playerPoints === 21) {
-            dispatch({type: "WIN"})
-        } else if(state.dealerPoints === 21) {
-            dispatch({type: "LOST"})
-        } else if(state.dealerPoints > 21) {
-            dispatch({type: "WIN"})
-        } else if((state.dealerPoints > state.playerPoints) && state.playerPoints <= 18){
-            dispatch({type: "LOST"})
+            props.lost()
+        } else if(props.playerPoints === 21) {
+            props.win()
+        } else if(props.dealerPoints === 21) {
+            props.lost()
+        } else if(props.dealerPoints > 21) {
+            props.win()
+        } else if((props.dealerPoints > props.playerPoints) && props.playerPoints <= 18){
+            props.lost()
         }
 
-    }, [state.cardPlayer, state.playerPoints, state.dealerPoints, state.cardDealer, state.aceValue])
-
-
-    const newGame = () => {
-        dispatch({type: "NEW_GAME"})
-        dispatch({type: "CARD_DISTRIBUTOR", persona: "player", number: "two", newCard: cardDistributor(2)})
     }
 
 
-    const stopHandler = useCallback(() => {
+    const newGame = () => {
+        props.newGame()
+        props.cardDHandler("player", "two", cardDistributor(2))
+    }
+
+
+    const stopHandler = () => {
         console.log("stop")
-        if (state.dealerPoints < 10) {
-            dispatch({type: "CARD_DISTRIBUTOR", persona: "dealer", number: "one", newCard: cardDistributor(1)});
-            if(state.dealerPoints < 12) {
-                dispatch({type: "CARD_DISTRIBUTOR", persona: "dealer", number: "one", newCard: cardDistributor(1)});
+        if (props.dealerPoints < 10) {
+            props.cardDHandler("dealer", "one", cardDistributor(1))
+            if(props.dealerPoints < 12) {
+                props.cardDHandler("dealer", "one", cardDistributor(1))
             }
-        } else if (state.dealerPoints <= 13) {
-            dispatch({type: "CARD_DISTRIBUTOR", persona: "dealer", number: "one", newCard: cardDistributor(1)});
-            if(state.dealerPoints < 15) {
-                dispatch({type: "CARD_DISTRIBUTOR", persona: "dealer", number: "one", newCard: cardDistributor(1)});
+        } else if (props.dealerPoints <= 13) {
+            props.cardDHandler("dealer", "one", cardDistributor(1))
+            if(props.dealerPoints < 15) {
+                props.cardDHandler("dealer", "one", cardDistributor(1))
             }
-        } else if(state.dealerPoints >= 15 && state.dealerPoints < state.playerPoints) {
-            dispatch({type: "FINISHED", payload: true})
-            dispatch({type: "CARD_DISTRIBUTOR", persona: "dealer", number: "one", newCard: cardDistributor(1)});
-        } else if(state.dealerPoints >= 21) {
-            dispatch({type: "FINISHED", payload: true})
+        } else if(props.dealerPoints >= 15 && props.dealerPoints < props.playerPoints) {
+            props.finishedHandler(true)
+            props.cardDHandler("dealer", "one", cardDistributor(1))
+        } else if(props.dealerPoints >= 21) {
+            props.finishedHandler(true)
         }
-    }, [state.dealerPoints, state.playerPoints, cardDistributor, state.cardDealer, state.cardPlayer])
+    }
     
     useEffect(() => {
-        playerPointsHandler();
+        //playerPointsHandler();
         console.log("ueffect")
-    }, [state.cardPlayer, playerPointsHandler, state.cardDealer, state.dealerPoints, stopHandler])
+    }, [props.cardPlayer, playerPointsHandler, props.cardDealer, props.dealerPoints, stopHandler])
 
 
     let banner = null;
-    if(state.gameFinished) {
-        banner = <Banner mess={state.message} amount={state.playerBet}/>
+    if(props.gameFinished) {
+        banner = <Banner mess={props.message} amount={props.playerBet}/>
     }
     return (
         <div className="board__container">
@@ -106,25 +103,25 @@ const Board = (props) => {
                 <div className="player__board">
                     <div>
                         <h3>You</h3>
-                    <p>💰 Money available: <span className="moneyAvailable">{state.playerMoney}$</span></p>
+                    <p>💰 Money available: <span className="moneyAvailable">{props.playerMoney}$</span></p>
                     </div>
                     <div className="card__container">
-                        {state.cardPlayer.map((c, index) => {
+                        {props.cardPlayer.map((c, index) => {
                             return <Card
-                                aceClicked={state.aceClicked} 
-                                addAce={dispatch({type: "ACE_HANDLER"})}
+                                aceClicked={props.aceClicked} 
+                                addAce={() => props.aceHandler(11)}
                                 key={index} 
                                 cardValues={c.cardVal} 
                                 suits={c.suits}/>
                         })}
                     </div>
                     <div className="player__button">
-                        <h2>Acutal Points: {state.playerPoints}</h2>
+                        <h2>Acutal Points: {props.playerPoints}</h2>
                         <div>
-                        <button disabled={state.gameFinished} onClick={() => stopHandler}>💲 Stop</button>
+                        <button disabled={props.gameFinished} onClick={() => stopHandler}>💲 Stop</button>
                         <label>Amount:</label>
-                        <input disabled={!state.gameFinished} value={state.playerBet} onChange={(e) => dispatch({type: "BET", payload: e.target.value})} type="number" step="25" placeholder="Insert money" min="0"/>
-                        <button onClick={() => dispatch({type: "CARD_DISTRIBUTOR", persona: "player", number: "one", newCard: cardDistributor(1)})} disabled={state.aceAppeard || state.gameFinished}>Card</button>
+                        <input disabled={!props.gameFinished} value={props.playerBet} onChange={(e) => props.bet(e.target.value)} type="number" step="25" placeholder="Insert money" min="0"/>
+                        <button onClick={() => props.cardDHandler("player", "one", cardDistributor(1))} disabled={props.aceAppeard || props.gameFinished}>Card</button>
                         <button onClick={() => newGame}>New Game</button>
                         </div>
 
@@ -133,20 +130,20 @@ const Board = (props) => {
                 <div className="dealer__board">
                     <div>
                         <h3>The Dealer</h3>
-                        <p>💰 Cash available in the bank: <span className="moneyAvailable">{state.dealerMoney}$</span></p>
+                        <p>💰 Cash available in the bank: <span className="moneyAvailable">{props.dealerMoney}$</span></p>
                     </div>
                     <div className="card__container">
-                    {state.cardDealer.map((c, index) => {
+                    {props.cardDealer.map((c, index) => {
                             return <Card
-                                aceClicked={state.aceClicked} 
-                                addAce={dispatch({type: "ACE_HANDLER"})} 
+                                aceClicked={props.aceClicked} 
+                                addAce={() => props.aceHandler(11)} 
                                 key={index} 
                                 cardValues={c.cardVal} 
                                 suits={c.suits}/>
                         })}
                         </div>
                     <div className="dealer__button">
-                        <h2>Actual Points: {state.dealerPoints}</h2>
+                        <h2>Actual Points: {props.dealerPoints}</h2>
                     </div>
                 </div>
             </div>
@@ -154,4 +151,46 @@ const Board = (props) => {
     );
 }
 
-export default Board;
+const mapStateToProps = state => {
+    return {
+        gameFinished: state.gameFinished,
+        playerMoney: state.playerMoney,
+        dealerMoney: state.dealerMoney,
+        message: state.message,
+        playerBet: state.playerBet,
+        aceAppeard: state.aceAppeard,
+        aceValue: state.aceValue,
+        aceClicked: state.aceClicked,
+        playerPoints: state.playerPoints,
+        dealerPoints: state.dealerPoints,
+        finished: state.finished,
+        suits: state.suits,
+        cardValues: state.cardValues,
+        cardPlayer: state.cardPlayer,
+        cardDealer: state.cardDealer
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        bet: (p) => dispatch({type: "BET", payload: p}),
+        newGame: () => dispatch({type: "NEW_GAME"}),
+        win: () => dispatch({type: "WIN"}),
+        lost: () => dispatch({type: "LOST"}),
+        aceAppeardHandler: () => dispatch({type: "ACE_APPEARED"}),
+        aceValueHandler: (d) => dispatch({type: "ACE_VALUE", payload: d}),
+        aceClickedHandler: () => dispatch({type: "ACE_CLICKED"}),
+        aceHandler: (p) => dispatch({type: "ACE_HANDLER", payload: p}),
+        playerPointsHandler: (p) => dispatch({type: "PLAYER_POINTS", payload: p}),
+        dealerPointsHandler: (p) => dispatch({type: "DEALER_POINTS", payload: p}),
+        finishedHandler: (p) => dispatch({type: "FINISHED", payload: p}),
+        cardPlayerHandler: (p) => dispatch({type: "CARD_PLAYER", payload: p}),
+        cardDealerHandler: (p) => dispatch({type: "CARD_DEALER", payload: p}),
+        cardDHandler: (p, n, nC) => dispatch({type: "CARD_DISTRIBUTOR", persona: p, number: n, newCard: nC}),
+
+    }
+}
+
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Board);
--- fullstack-blackjack/src/index.js
+++ fullstack-blackjack/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {createStore} from "redux";
+import {createStore, compose} from "redux";
 import {Provider} from "react-redux";
 import reducer from "./store/board"
 
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+
+const store = createStore(reducer, composeEnhancers(
+  //
+));
 
 ReactDOM.render(
   <Provider store={store}>
