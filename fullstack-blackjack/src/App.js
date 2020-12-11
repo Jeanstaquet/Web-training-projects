74980665421fbd283c0ebfe51896b87be27dcac0
Jeansta <jean.staquet8@gmail.com>
2020-12-11 18:04:07 +0100

    Ajout des routes pour changer de page entre l'auth et le reste

--- fullstack-blackjack/src/App.js
+++ fullstack-blackjack/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, {useState} from "react";
 import Board from "./containers/Board/Board";
 import Auth from "./components/Auth/Auth";
+import {Route, Switch, Redirect } from "react-router-dom";
 
 const App = () => {
   const [type, setType] = useState("Signin");
@@ -16,8 +17,12 @@ const App = () => {
 
   return (
     <div className="App">
-      <Auth type={type} click={loginHandler}/>
-      {/* <Board/> */}
+      <Switch>
+        <Route path="/auth">
+          <Auth type={type} click={loginHandler}/>
+        </Route>
+        <Route exact path="/" component={Board}></Route>
+      </Switch>
     </div>
   );
 }
