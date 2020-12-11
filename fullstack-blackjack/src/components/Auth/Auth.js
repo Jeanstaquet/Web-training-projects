6b8004df32ea673effad918d22bc62891a04e357
Jeansta <jean.staquet8@gmail.com>
2020-12-11 21:30:32 +0100

    La query passe et on est enregistrer sur le fb

--- fullstack-blackjack/src/components/Auth/Auth.js
+++ fullstack-blackjack/src/components/Auth/Auth.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Auth.scss";
 import {connect} from "react-redux";
 import * as actions from "../../store/actions/index"
 
 function Auth(props) {
     
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+ 
+    const registerHandler = (e, email, password) => {
+        e.preventDefault();
+
+        props.onAuth(email, password);
+    }
+
+
     let ui = <div className="auth__page">
             <form className="auth__container">
                 <h3>Add your information to register</h3>
                 <label>Email</label>
-                <input type="email" name="email" placeholder="Type your email here 🚀"/>
+                <input type="email" name="email" onChange={e => setEmail(e.target.value)} placeholder="Type your email here 🚀"/>
                 <label>Password</label>
-                <input type="password" name="password" placeholder="Type your password here ! 🚀"/>
+                <input type="password" onChange={e => setPassword(e.target.value)} name="password" placeholder="Type your password here ! 🚀"/>
                 <label>Re-type your password</label>
                 <input type="password" placeholder="Type your password here !  🚀"/>
-                <button type="submit" onClick={(e) => e.preventDefault()}>Register</button>
+                <button type="submit" onClick={(e) => registerHandler(e, email, password)}>Register</button>
                 <button type="submit" onClick={(e) => e.preventDefault()}>Register with whatsapp</button>
                 <p onClick={() => props.click()}>Already an account ? Choose to log-in instead</p>
             </form>
@@ -39,6 +49,17 @@ function Auth(props) {
     );
 }
 
+const mapStateToProps = state => {
+    return {
+        loading: state.auth.loading
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onAuth: (e, p) => dispatch(actions.auth(e, p)) 
+    }
+}
 
 
-export default Auth;
+export default connect(mapStateToProps, mapDispatchToProps)(Auth);
