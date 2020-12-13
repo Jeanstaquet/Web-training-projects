3d8fa9054152a82eb21b52de28c124d06c1be245
Jeansta <jean.staquet8@gmail.com>
2020-12-14 00:49:25 +0100

    Setting up redux for the auth

--- whatsapp-revisited-clone/src/index.js
+++ whatsapp-revisited-clone/src/index.js
@@ -3,12 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom"
+import {BrowserRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import authReducer from "./store/reducer/auth";
+import { createStore, applyMiddleware, compose} from "redux";
+import thunk from "redux-thunk";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(authReducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
+
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
