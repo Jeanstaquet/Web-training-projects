677e06d6329986eb105aa50068ed83b8fcfda5cd
Jeansta <jean.staquet8@gmail.com>
2020-12-13 14:35:29 +0100

    Just did the chat banner styling

--- whatsapp-revisited-clone/src/containers/Chat/Chat.js
+++ whatsapp-revisited-clone/src/containers/Chat/Chat.js
@@ -5,16 +5,20 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
 import AttachFileIcon from '@material-ui/icons/AttachFile';
 import MicIcon from '@material-ui/icons/Mic';
+import { Avatar } from '@material-ui/core';
 const Chat = () => {
     return (
         <div className="chat__container">
             <div className="chat__banner">
                 <div className="chat__bannerInfo">
-                    <p>img</p>
-                    <p>Name</p>
-                    <p>Person</p>
+                    <Avatar className="chat__bannerAvatar"/>
+                    <div className="chat__info">
+                        <p>Name</p>
+                        <p>Person</p>
+                    </div>
+
                 </div>
-                <div className="chat__banner ">
+                <div className="chat__bannerIcon">
                     <SearchIcon/>
                     <MoreHorizIcon/>
                 </div>
