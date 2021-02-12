e1367da3970777908aff73ddbb969f450769396e
Jeansta <jean.staquet8@gmail.com>
2021-02-12 23:17:44 +0100

    Init du projet, néttoyementdes fichiers initiaux de react

--- Practice/controllers/admin.js
+++ Practice/controllers/admin.js
@@ -13,13 +13,13 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  console.log(title, imageUrl, price, description)
-  Product.create({
+  req.user.createProduct({
     title: title,
     price: price,
     imageUrl: imageUrl,
-    description: description
-  }).then(res => res.redirect("/admin/products"))
+    description: description,
+  })
+    .then(resp => res.redirect("/admin/products"))
     .catch(err => console.log(err))
 };
 
@@ -29,8 +29,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findByPk(prodId)
-    .then(product => {
+  req.user.getProducts({where: {id: prodId}})
+  // Product.findByPk(prodId)
+    .then(products => {
+      const product = products[0]
       res.render('admin/edit-product', {
         pageTitle: 'Edit Product',
         path: '/admin/edit-product',
@@ -60,7 +62,7 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user.getProducts()
   .then(product => {
     res.render('admin/products', {
       prods: product,
