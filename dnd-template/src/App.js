4d21e9545cce92885f590e26829111de769e9e61
Jeansta <jean.staquet8@gmail.com>
2021-02-13 02:51:13 +0100

    Maj du projet

--- dnd-template/src/App.js
+++ dnd-template/src/App.js
@@ -1,30 +1,9 @@
 import React from 'react';
-import "./App.css";
-
-import Board from "./components/Board";
-import Card from "./components/Card";
-
 
 const App = () => {
   return (
     <div>
-      <main className="flexbox">
-        <Board id="board-1" className="board">
-            <Card id="card-1" className="card" draggable='true'>
-                <p>Card One</p>
-              </Card>
-        </Board>
-
-        <Board id="board-2" className="board">
-            <Card id="card-2" className="card" draggable='true'>
-                <p>Card Two</p>
-              </Card>
-        </Board>
-
-        <Board id="board-2" className="board">
-
-        </Board>
-      </main>
+      
     </div>
   );
 };
