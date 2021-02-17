e4143173866ad5f69ce233beece437bbf36dd915
Jeansta <jean.staquet8@gmail.com>
2021-02-17 22:39:34 +0100

    card creation works when it's not done with the custom hook

--- trello-clone/client/src/containers/Board/Board.js
+++ trello-clone/client/src/containers/Board/Board.js
@@ -8,9 +8,51 @@ import {useAppDispatch} from "../../Context/index";
 import {useAppData} from "../../Context/index";
 import axios from "axios";
 import { v4 as uuidv4 } from 'uuid';
+
+const item = {
+    id: "zefzef",
+    name: "Clean the house",
+    tags: "Urgent S.O.S."
+  }
+
+  const item2 = {
+    id: "zefze",
+    name: "Wash the car"
+  }
+
+  const item3 = {
+    id: "eifjefd",
+    name: "Go forward in this project"
+  }
+
+  const item4 = {
+    id: "eifjzsefd",
+    name: "Go forzszsward in this project"
+  }
+
+  const item5 = {
+    id: "zdzjefd",
+    name: "zdszorward in this project"
+  }
+
+const dataCol = {
+    0: {
+        title: "Todo",
+        items: [item, item2]
+      },
+    1: {
+        title: "In Progress",
+        items: [item5]
+      },
+    2: {
+        title: "Completed",
+        items: [item3, item4]}
+}
+
 const Board = () => {
   const state = useAppData()
   const [openModal, setOpenModal] = useState(false)
+  const [data, setData] = useState(dataCol)
   const {updateCol, newState, setCol} = useAppDispatch()
   const [modifiedCol, setModifiedCol] = useState({})
   const [modalDescription, setModalDescription] = useState("")
@@ -26,12 +68,12 @@ const Board = () => {
     }
   }
 
-  useEffect(() => {
-      axios.get("http://localhost:5000/")
-      .then((result) => {
-        newState({...result.data})
-      })
-  }, [])
+  // useEffect(() => {
+  //     axios.get("http://localhost:5000/")
+  //     .then((result) => {
+  //       newState({...result.data})
+  //     })
+  // }, [])
 
   const handleDragEnd = ({destination, source}) => {
     if(!destination) {
@@ -43,46 +85,53 @@ const Board = () => {
       return
     }
 
-    return updateCol(source.droppableId, source.index, destination.droppableId, destination.index)
+    //return updateCol(source.droppableId, source.index, destination.droppableId, destination.index)
     
 
-    // const itemCopy = {...data[source.droppableId].items[source.index]}
-    // setData(prev => {
-    //   prev = {...prev}
-    //   // Remove from previous items array
-    //   prev[source.droppableId].items.splice(source.index, 1)
+    const itemCopy = {...data[source.droppableId].items[source.index]}
+    setData(prev => {
+      prev = {...prev}
+      // Remove from previous items array
+      prev[source.droppableId].items.splice(source.index, 1)
 
 
-    //   // Adding to new items array location
-    //   prev[destination.droppableId].items.splice(destination.index, 0, itemCopy)
+      // Adding to new items array location
+      prev[destination.droppableId].items.splice(destination.index, 0, itemCopy)
 
-    //   return prev
-    // })
+      return prev
+    })
     
-
+    
   }
+  
   const handleColumnName = (colNbr, data) => {
     setOpenModal(true);
     setCol(data, colNbr)
-
   }
   const changeDesModal = (des) => {
     setModalDescription(des)
   }
-
   const saveNewElement = (description) => {
-    const previousitem = [...state.changedCol.items]
-    previousitem.push({id: uuidv4(), name: description})
+    const prevItems = [...state.changedCol.items, {id: uuidv4(), name: description}];
+    const neS = {...data}
+    neS[state.index].items = prevItems;
+    setData(neS)
+     
+    // console.log("debut", [...state.changedCol.items])
+    // const previousitem = [...state.changedCol.items, {_id: uuidv4(), id: uuidv4(), name: description}]
+    // console.log("fin", previousitem)
 
-    const nS = {...state}
-    nS[state.index].items = previousitem
-    delete nS.index
-    delete nS.changedCol
-    setOpenModal(false);
-    setModalDescription(" ")
-    newState(nS)
+    // const nS = {...state}
+    // nS[state.index].items = previousitem;
+    // delete nS.index;
+    // delete nS.changedCol;
+    // setOpenModal(false);
+    // setModalDescription("")
+    // newState(nS)
+
+    console.log(state)
   }
-
+  console.log(data)
     return (
         <div className="board">
             <Navigation/>
@@ -94,8 +143,8 @@ const Board = () => {
               title={modifiedCol.title}
               save={() => saveNewElement(modalDescription)}/>
             <DragDropContext onDragEnd={handleDragEnd}>
-              {state ? 
-                 Object.entries(state).map(([key, val]) => {
+              {data ? 
+                 Object.entries(data).map(([key, val]) => {
                   return <Column click={handleColumnName} title={val.title} key={key} data={val} id={key}/>
                 })
               : null}
