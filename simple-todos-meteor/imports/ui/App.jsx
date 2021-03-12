c4723c7b830bde8c0d2fa2f28aac1922dd3be7b1
Jeanstaquet <jean.staquet8@gmail.com>
2021-03-12 10:45:39 -0800

    Init the project with meteor

---
+++ simple-todos-meteor/imports/ui/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Task } from './Task';
+
+const tasks = [
+  {_id: 1, text: 'First Task'},
+  {_id: 2, text: 'Second Task'},
+  {_id: 3, text: 'Third Task'},
+];
+
+export const App = () => (
+  <div>
+    <h1>Welcome to Meteor!</h1>
+    
+  </div>
+);
