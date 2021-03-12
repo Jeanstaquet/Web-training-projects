777a62b9a42d00ab6fc2b8c6f470495e48b09dd6
Jeanstaquet <jean.staquet8@gmail.com>
2021-03-12 13:45:10 -0800

    Add the logic for adding and removing a task

--- simple-todos-meteor/imports/ui/App.jsx
+++ simple-todos-meteor/imports/ui/App.jsx
@@ -1,15 +1,47 @@
 import React from 'react';
 import { Task } from './Task';
+import { TasksCollection } from "../../imports/api/TasksCollection";
+//Enables the reactivity for the data
+import { useTracker } from 'meteor/react-meteor-data';
+import { TaskForm } from "./TaskForm";
 
-const tasks = [
-  {_id: 1, text: 'First Task'},
-  {_id: 2, text: 'Second Task'},
-  {_id: 3, text: 'Third Task'},
-];
+const toggleChecked = ({ _id, isChecked }) => {
+  TasksCollection.update(_id, {
+    $set: {
+      isChecked: !isChecked
+    }
+  })
+};
 
-export const App = () => (
-  <div>
-    <h1>Welcome to Meteor!</h1>
-    
-  </div>
-);
+const deleteTask = ({ _id }) => TasksCollection.remove(_id);
+
+export const App = () => {
+  const tasks = useTracker(() => TasksCollection.find({}, { sort: { createdAt: -1 } }).fetch());
+
+  return (
+    <div className="app">
+      <header>
+        <div className="app-bar">
+          <div className="app-header">
+            <h1>Welcome to Meteor!</h1>
+          </div>
+        </div>
+      </header>
+
+      <div className="main">
+        <TaskForm />
+
+        <ul className="tasks">
+          {tasks.map(task => (
+            <Task
+              key={task._id}
+              task={task}
+              onCheckboxClick={toggleChecked}
+              onDeleteClick={deleteTask}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
